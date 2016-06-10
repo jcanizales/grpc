932777078b442095acf3446adeecf950a3b22682
Jorge Canizales <jcanizales@google.com>
2016-06-10 15:53:22 -0700

    Add new E2ETests test target

---
+++ src/objective-c/tests/E2ETests/E2ETests.m
@@ -0,0 +1,39 @@
+//
+//  E2ETests.m
+//  E2ETests
+//
+//  Created by Jorge Canizalez Diaz on 6/10/16.
+//  Copyright © 2016 gRPC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface E2ETests : XCTestCase
+
+@end
+
+@implementation E2ETests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
